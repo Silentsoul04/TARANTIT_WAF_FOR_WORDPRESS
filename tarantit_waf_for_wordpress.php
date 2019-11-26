422676121acb84ce6be941f709655033d2b7306b
Friedrich <cm@itfinden.com>
2019-11-26 14:14:21 -0300

    version 1.2

--- tarantit_waf_for_wordpress.php
+++ tarantit_waf_for_wordpress.php
@@ -1,36 +1,36 @@
 <?php
 /**
- * WP Shieldon
+ * TARANTIT WAF FOR WORDPRESS
  *
- * @author Terry Lin
- * @link https://terryl.in/
+ * @author TARANTIT INC
+ * @link https://www.tarantit.com
  *
- * @package Shieldon
+ * @package ITFINDEN
  * @since 1.0.0
  * @version 1.4.1
  */
 
 /**
- * Plugin Name: WP Shieldon
- * Plugin URI:  https://github.com/terrylinooo/wp-shieldon
+ * Plugin Name: TARANTIT WAF FOR WORDPRESS
+ * Plugin URI:  https://github.com/terrylinooo/wp-ITFINDEN
  * Description: An anti-scraping plugin for WordPress.
  * Version:     1.4.1
- * Author:      Terry Lin
- * Author URI:  https://terryl.in/
+ * Author:      Tarantit
+ * Author URI:  https://www.itfinden.com/
  * License:     GPL 3.0
  * License URI: http://www.gnu.org/licenses/gpl-3.0.txt
- * Text Domain: wp-shieldon
+ * Text Domain: wp-ITFINDEN
  * Domain Path: /languages
  */
 
 /**
  * Any issues, or would like to request a feature, please visit.
- * https://github.com/terrylinooo/wp-shieldon/issues
+ * https://github.com/terrylinooo/wp-ITFINDEN/issues
  * 
  * Welcome to contribute your code here:
- * https://github.com/terrylinooo/wp-shieldon
+ * https://github.com/terrylinooo/wp-ITFINDEN
  *
- * Thanks for using WP WP Shieldon!
+ * Thanks for using WP WP ITFINDEN!
  * Star it, fork it, share it if you like this plugin.
  */
 
@@ -43,47 +43,47 @@ if ( ! defined( 'WPINC' ) ) {
  * 
  * Those below constants will be assigned to: `/Controllers/ControllerAstruct.php`
  * 
- * SHIELDON_PLUGIN_NAME          : Plugin's name.
- * SHIELDON_PLUGIN_DIR           : The absolute path of the WP Shieldon plugin directory.
- * SHIELDON_PLUGIN_URL           : The URL of the WP Shieldon plugin directory.
- * SHIELDON_PLUGIN_PATH          : The absolute path of the WP Shieldon plugin launcher.
- * SHIELDON_PLUGIN_LANGUAGE_PACK : Translation Language pack.
- * SHIELDON_PLUGIN_VERSION       : WP Shieldon plugin version number
- * SHIELDON_PLUGIN_TEXT_DOMAIN   : WP Shieldon plugin text domain
+ * ITFINDEN_PLUGIN_NAME          : Plugin's name.
+ * ITFINDEN_PLUGIN_DIR           : The absolute path of the WP ITFINDEN plugin directory.
+ * ITFINDEN_PLUGIN_URL           : The URL of the WP ITFINDEN plugin directory.
+ * ITFINDEN_PLUGIN_PATH          : The absolute path of the WP ITFINDEN plugin launcher.
+ * ITFINDEN_PLUGIN_LANGUAGE_PACK : Translation Language pack.
+ * ITFINDEN_PLUGIN_VERSION       : WP ITFINDEN plugin version number
+ * ITFINDEN_PLUGIN_TEXT_DOMAIN   : WP ITFINDEN plugin text domain
  * 
  * Expected values:
  * 
- * SHIELDON_PLUGIN_DIR           : {absolute_path}/wp-content/plugins/wp-shieldon/
- * SHIELDON_PLUGIN_URL           : {protocal}://{domain_name}/wp-content/plugins/wp-shieldon/
- * SHIELDON_PLUGIN_PATH          : {absolute_path}/wp-content/plugins/wp-shieldon/wp-shieldon.php
- * SHIELDON_PLUGIN_LANGUAGE_PACK : wp-shieldon/languages
+ * ITFINDEN_PLUGIN_DIR           : {absolute_path}/wp-content/plugins/wp-ITFINDEN/
+ * ITFINDEN_PLUGIN_URL           : {protocal}://{domain_name}/wp-content/plugins/wp-ITFINDEN/
+ * ITFINDEN_PLUGIN_PATH          : {absolute_path}/wp-content/plugins/wp-ITFINDEN/wp-ITFINDEN.php
+ * ITFINDEN_PLUGIN_LANGUAGE_PACK : wp-ITFINDEN/languages
  */
 
-define( 'SHIELDON_PLUGIN_NAME', plugin_basename( __FILE__ ) );
-define( 'SHIELDON_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
-define( 'SHIELDON_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
-define( 'SHIELDON_PLUGIN_PATH', __FILE__ );
-define( 'SHIELDON_PLUGIN_LANGUAGE_PACK', dirname( plugin_basename( __FILE__ ) ) . '/languages' );
-define( 'SHIELDON_PLUGIN_VERSION', '1.4.1' );
-define( 'SHIELDON_CORE_VERSION', '0.1.3' );
-define( 'SHIELDON_PLUGIN_TEXT_DOMAIN', 'wp-shieldon' );
+define( 'ITFINDEN_PLUGIN_NAME', plugin_basename( __FILE__ ) );
+define( 'ITFINDEN_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+define( 'ITFINDEN_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+define( 'ITFINDEN_PLUGIN_PATH', __FILE__ );
+define( 'ITFINDEN_PLUGIN_LANGUAGE_PACK', dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+define( 'ITFINDEN_PLUGIN_VERSION', '1.4.1' );
+define( 'ITFINDEN_CORE_VERSION', '0.1.3' );
+define( 'ITFINDEN_PLUGIN_TEXT_DOMAIN', 'wp-ITFINDEN' );
 
 // Load helper functions
-require_once SHIELDON_PLUGIN_DIR . 'src/wpso-helper-functions.php';
+require_once ITFINDEN_PLUGIN_DIR . 'src/wpso-helper-functions.php';
 
 // Load language packs.
 add_action( 'init', 'wpso_load_textdomain' );
 
-// Composer autoloader. Mainly load Shieldon library.
-require_once SHIELDON_PLUGIN_DIR . 'vendor/autoload.php';
+// Composer autoloader. Mainly load ITFINDEN library.
+require_once ITFINDEN_PLUGIN_DIR . 'vendor/autoload.php';
 
-// WP Shieldon Class autoloader.
-require_once SHIELDON_PLUGIN_DIR . 'src/autoload.php';
+// WP ITFINDEN Class autoloader.
+require_once ITFINDEN_PLUGIN_DIR . 'src/autoload.php';
 
 if ( version_compare( phpversion(), '7.1.0', '>=' ) ) {
 
 	/**
-	 * Activate Shieldon plugin.
+	 * Activate ITFINDEN plugin.
 	 */
 	function wpso_activate_plugin() {
 
@@ -91,7 +91,7 @@ if ( version_compare( phpversion(), '7.1.0', '>=' ) ) {
 
 		update_option( 'wpso_lang_code', substr( get_locale(), 0, 2 ) );
 		update_option( 'wpso_last_reset_time', time() );
-		update_option( 'wpso_version', SHIELDON_PLUGIN_VERSION );
+		update_option( 'wpso_version', ITFINDEN_PLUGIN_VERSION );
 
 		// Add default setting. Only execute this action at the first time activation.
 		if ( false === wpso_is_driver_hash() ) {
@@ -103,12 +103,12 @@ if ( version_compare( phpversion(), '7.1.0', '>=' ) ) {
 
 				$files = array(
 					array(
-						'base'    => WP_CONTENT_DIR . '/uploads/wp-shieldon',
+						'base'    => WP_CONTENT_DIR . '/uploads/wp-ITFINDEN',
 						'file'    => 'index.html',
 						'content' => '',
 					),
 					array(
-						'base'    => WP_CONTENT_DIR . '/uploads/wp-shieldon',
+						'base'    => WP_CONTENT_DIR . '/uploads/wp-ITFINDEN',
 						'file'    => '.htaccess',
 						'content' => 'deny from all',
 					),
@@ -129,13 +129,13 @@ if ( version_compare( phpversion(), '7.1.0', '>=' ) ) {
 	}
 
 	/**
-	 * Deactivate Shieldon plugin.
+	 * Deactivate ITFINDEN plugin.
 	 *
 	 */
 	function wpso_deactivate_plugin() {
 		$dir = wpso_get_upload_dir();
 
-		//  Remove all files created by WP Shieldon plugin.
+		//  Remove all files created by WP ITFINDEN plugin.
 		if ( file_exists( $dir ) ) {
 			$it    = new RecursiveDirectoryIterator( $dir, RecursiveDirectoryIterator::SKIP_DOTS );
 			$files = new RecursiveIteratorIterator( $it, RecursiveIteratorIterator::CHILD_FIRST );
@@ -169,16 +169,16 @@ if ( version_compare( phpversion(), '7.1.0', '>=' ) ) {
 	register_deactivation_hook( __FILE__, 'wpso_deactivate_plugin' );
 
 	/**
-	 * Start to run WP Shieldon plugin cores.
+	 * Start to run WP ITFINDEN plugin cores.
 	 */
 	if ( is_admin() ) {
 
 		// Check version.
 		$wpso_version = get_option( 'wpso_version' );
 
-		if ( $wpso_version < SHIELDON_PLUGIN_VERSION ) {
-			wpso_set_option( 'enable_daemon', 'shieldon_daemon', 'no' );
-			update_option( 'wpso_version', SHIELDON_PLUGIN_VERSION );
+		if ( $wpso_version < ITFINDEN_PLUGIN_VERSION ) {
+			wpso_set_option( 'enable_daemon', 'ITFINDEN_daemon', 'no' );
+			update_option( 'wpso_version', ITFINDEN_PLUGIN_VERSION );
 			add_action( 'admin_notices', 'wpso_update_notice' );
 		}
 
@@ -190,11 +190,11 @@ if ( version_compare( phpversion(), '7.1.0', '>=' ) ) {
 		add_action( 'admin_init', array( $admin_ip_manager, 'setting_admin_init' ) );
 		add_action( 'admin_menu', array( $admin_menu, 'setting_admin_menu' ) );
 		add_filter( 'admin_body_class', array( $admin_settings, 'setting_admin_body_class' ) );
-		add_filter( 'plugin_action_links_' . SHIELDON_PLUGIN_NAME, array( $admin_menu, 'plugin_action_links' ), 10, 5 );
+		add_filter( 'plugin_action_links_' . ITFINDEN_PLUGIN_NAME, array( $admin_menu, 'plugin_action_links' ), 10, 5 );
 		add_filter( 'plugin_row_meta', array( $admin_menu, 'plugin_extend_links' ), 10, 2 );
 
 		// If we detect the setting changes.
-		if ( ! empty( $_POST['shieldon_daemon[data_driver_type]'] ) ) {
+		if ( ! empty( $_POST['ITFINDEN_daemon[data_driver_type]'] ) ) {
 			update_option( 'wpso_driver_reset', 'yes' );
 		}
 
@@ -203,10 +203,10 @@ if ( version_compare( phpversion(), '7.1.0', '>=' ) ) {
 
 	} else {
 
-		if ( 'yes' === wpso_get_option( 'enable_daemon', 'shieldon_daemon' ) ) {
+		if ( 'yes' === wpso_get_option( 'enable_daemon', 'ITFINDEN_daemon' ) ) {
 
 			/**
-			 * Shieldon daemon.
+			 * ITFINDEN daemon.
 			 *
 			 * @return void
 			 */
@@ -217,7 +217,7 @@ if ( version_compare( phpversion(), '7.1.0', '>=' ) ) {
 				$guardian->run();
 			}
 
-			// Load main launcher class of WP Shieldon plugin at a very early hook.
+			// Load main launcher class of WP ITFINDEN plugin at a very early hook.
 			add_action( 'plugins_loaded', 'wpso_init', -100 );
 		}
 	}
